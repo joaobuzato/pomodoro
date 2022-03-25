aa13dd34400414db644d5e23076cd01ff03c2d12
Buzato <joaobuzato@gmail.com>
2022-03-25 18:02:04 -0300

    updating user UI - adding time behaviour

--- main.py
+++ main.py
@@ -13,9 +13,15 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(10)
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
+def count_down(count):
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        window.after(1000, count_down, count -1)
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -24,17 +30,17 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=228, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 
 title = Label(text="Timer", font=(FONT_NAME, 45, "bold"), fg=GREEN, bg=YELLOW)
 title.grid(column=1, row=0)
 
-start_button = Button(text="Start") # add command
+start_button = Button(text="Start", command=start_timer) # add command
 reset_button = Button(text="Reset") # add command
 start_button.grid(column=0, row=2)
 reset_button.grid(column=2, row=2)
 
-check_marks = Label(text=CHECK_MARK, fg=GREEN, bg=YELLOW)
+check_marks = Label(text=CHECK_MARK, fg=GREEN, bg=YELLOW, font=(FONT_NAME, 45, "bold"))
 check_marks.grid(column=1, row=3)
 window.mainloop()
