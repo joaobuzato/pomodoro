7f67cad18d5031f842de8d8502b90b3d20ccb7d1
Buzato <joaobuzato@gmail.com>
2022-03-25 09:33:02 -0300

    updating user UI

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
+CHECK_MARK="✔"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
@@ -24,7 +25,10 @@ canvas = Canvas(width=200, height=228, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
-canvas.grid(column=2, row=2)
+canvas.grid(column=1, row=1)
+
+title = Label(text="Timer", font=(FONT_NAME, 45, "bold"), fg=GREEN, bg=YELLOW)
+title.grid(column=1, row=0)
 
 
 
